de63730947b6043442211efa9aed28b2216d1b67
dwivedir <raghvendradubey9@gmail.com>
2014-08-28 00:26:59 +0530

    Added a little helper and stylesheets

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -8,22 +8,39 @@ RSpec.describe PagesController, :type => :controller do
       expect(response).to be_success
     end
     
-    it "return proper title" do
+    it "should have the right title" do
       get :home
       expect(response.body).to have_title("Home")
     end
+    
+    it "should have non-blank body" do
+      get :home
+      expect(response.body).not_to match(/<body>\s*<\/body>/)
+    end
   end
-
+   
   describe "GET contact" do
     it "returns http success" do
       get :contact
       expect(response).to be_success
     end
+  
     it "should return proper title" do
       get :contact
       expect(response.body).to have_title("Contact")
     end
   end
   
+  describe "GET About" do
+    it "returns http success" do
+      get :about
+      expect(response).to be_success
+    end
+
+    it "should return proper title" do
+      get :about
+      expect(response.body).to have_title("About")
+    end
+  end
 
 end
